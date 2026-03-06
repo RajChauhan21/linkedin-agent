4b720fc564cc4cb7f289b604098317dda0710b1e
RajChauhan21 <craj47577@gmail.com>
2026-03-06 23:04:31 +0530

    added retry mechanism

--- src/main/java/com/tutorial/ai/linkedin_agent/service/LinkedInAgentSchedulerService.java
+++ src/main/java/com/tutorial/ai/linkedin_agent/service/LinkedInAgentSchedulerService.java
@@ -4,6 +4,8 @@ import com.tutorial.ai.linkedin_agent.tools.ImageGenerationTools;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -19,6 +21,7 @@ public class LinkedInAgentSchedulerService {
     private ImageGenerationTools imageGenerationTools;
 
 
+    @Retryable(maxAttempts = 3,backoff = @Backoff(delay = 2000, multiplier = 2))
     @Scheduled(cron = "0 0 9 * * *", zone = "Asia/Kolkata") // 9 AM daily
     public void scheduledImageGeneration() {
         log.info("Starting scheduled image generation at: {}", System.currentTimeMillis());
